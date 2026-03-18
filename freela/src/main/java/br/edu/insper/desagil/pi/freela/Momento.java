3c81e9018c884a7d9e45b2f0e68520797bbfa293
anacarolinamartinezferreira <anacarolinamartinezferreira@gmail.com>
2026-03-18 18:25:44 -0300

    Mudando o diagrama

---
+++ freela/src/main/java/br/edu/insper/desagil/pi/freela/Momento.java
@@ -0,0 +1,14 @@
+
+public class Momento {
+    public int ajusta(int valor, int minimo, int maximo){
+        if(valor<minimo){
+            return minimo;
+        } else if (valor>maximo) {
+            return maximo;
+        } else{
+            return valor;
+        }
+    }
+
+    public int minutos(){}
+}
